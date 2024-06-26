f8ea0d198fbc6545a207d456d00b34dcc58f2d31
mobdim <dima@mobdim.com>
2024-06-27 00:28:37 +0300

    Add initial project

---
+++ TreeView/TreeView/TreeView.h
@@ -0,0 +1,18 @@
+//
+//  TreeView.h
+//  TreeView
+//
+//  Created by dima on 27.06.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TreeView.
+FOUNDATION_EXPORT double TreeViewVersionNumber;
+
+//! Project version string for TreeView.
+FOUNDATION_EXPORT const unsigned char TreeViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TreeView/PublicHeader.h>
+
+
